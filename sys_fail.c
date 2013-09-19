556b50c4a1e6734c85ed2f52fcf8c1d5338da207
prj2113 <prj2113@columbia.edu>
2013-09-19 19:46:40 -0400

    Testing out new ideas.

--- sys_fail.c
+++ sys_fail.c
@@ -1,13 +1,42 @@
-int sys_fail(int N)
+#include <linux/sched.h>
+
+struct task_struct *task;
+
+static int counter;
+static int n;
+static int sys_fail_ran = 0;
+
+asmlinkage long sys_fail(int N)
 {
-return 0;
+
+	counter = 0;
+	n = N;
+	sys_fail_ran = 1;
+	return 0;
+
 }
+
 long should_fail(void)
 {
-return 0;
-} 
+/*
+	if (sys_fail_ran == 1)
+	{
+		counter++;
+	}	
+	if (counter > 0)
+	{
+		return 1;
+	} else { return 0; }
+	if(( counter ) == n)
+		return 1;
+	else
+*/
+		return 0;
+}
+ 
+
 long fail_syscall(void)
 {
-return 0;
+	return 0;
 }
 
