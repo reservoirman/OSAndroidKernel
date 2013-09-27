1604412ce2e93a0309dfb7dce05c36357ffdc0eb
prj2113 <prj2113@columbia.edu>
2013-09-26 22:23:34 -0400

    Almost done\!

--- sys_fail.c
+++ sys_fail.c
@@ -1,16 +1,16 @@
 #include <linux/sched.h>
+#include <asm-generic/errno-base.h>
+#include <linux/syscalls.h>
+#include <asm/thread_info.h>
 
-struct task_struct *task;
 
-static int counter;
-static int n;
-static int sys_fail_ran = 0;
-
+int n;
+int sys_fail_ran = 0;
+ 
 asmlinkage long sys_fail(int N)
 {
-
-	counter = 0;
-	n = N;
+	current_thread_info()->task->hw2_count = 0;
+	n= N;
 	sys_fail_ran = 1;
 	return 0;
 
@@ -18,25 +18,32 @@ asmlinkage long sys_fail(int N)
 
 long should_fail(void)
 {
-/*
+	
 	if (sys_fail_ran == 1)
 	{
-		counter++;
-	}	
-	if (counter > 0)
-	{
-		return 1;
-	} else { return 0; }
-	if(( counter ) == n)
-		return 1;
+		current_thread_info()->task->hw2_count++;
+	
+		if(current_thread_info()->task->hw2_count < n)
+		{
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
+	}
 	else
-*/
+	{
 		return 0;
+	}
 }
  
 
 long fail_syscall(void)
 {
-	return 0;
+	sys_fail_ran = 0;
+	n=0;
+	current_thread_info()->task->hw2_count = 0;
+	return(-EINVAL);
 }
 
