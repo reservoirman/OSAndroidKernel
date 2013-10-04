ccd7c9d5b41e766e43a48aec265e16d2ba736541
prj2113 <prj2113@columbia.edu>
2013-10-04 09:28:35 -0400

    added the updated thread_test.cfile

--- thread_test.c
+++ thread_test.c
@@ -10,16 +10,17 @@
 int
 threadmain(void *arg)
 {
-  int threadno = (int)arg;
-  for (;;) {
-    printf("thread %d: I'm going to sleep\n", threadno);
-    sthread_suspend();
-    printf("thread %d: I woke up!\n", threadno);
-  }
-  return 0;
+	int threadno = (int)arg;
+	for (;;) 
+  	{
+    		printf("thread %d: I'm going to sleep\n", threadno);
+  		sthread_suspend();
+    		printf("thread %d: I woke up!\n", threadno);
+  	}
+  	return 0;
 }
 
- int balance = 0;
+int balance = 0;
 
 int deposit(void *arg)
 {
@@ -35,7 +36,7 @@ int deposit(void *arg)
 int withdraw(void *arg)
 { 
 	int i = 0;
-	//while (1)//
+	//while (1)
 	{
 		for (i = 0; i < 1000000000; i++)
 		balance--;
@@ -44,69 +45,84 @@ int withdraw(void *arg)
 }
 
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  sthread_t thr1, thr2, thr3, thr4, thr5;
+	sthread_t thr1, thr2, thr3, thr4, thr5;
 
-  // Test sthread_sem_init and sthread_sem_destroy 
-  sthread_sem_t test;
-  printf("test retrun val:%d\n", sthread_sem_init(&test, 3)); 
-  printf("test init:%d\n", test.count);
-  // printf("test return destroy:%d\n", sthread_sem_destroy(&test));
-  printf("test init:%d\n", test.count);
+     // Test sthread_sem_init and sthread_sem_destroy 
+ 	sthread_sem_t test;
+	test.sthread_count = 0;
+	printf("test retrun val:%d\n", sthread_sem_init(&test, 3)); 
+ 	printf("test init:%d\n", test.count);
+     //	printf("test return destroy:%d\n", sthread_sem_destroy(&test));
+ 	printf("test init:%d\n", test.count);
 
+ 	if (sthread_init() == -1)
+   		fprintf(stderr, "%s: sthread_init: %s\n", argv[0], strerror(errno));
 
+	if (sthread_create(&thr2, deposit, (void *)2) == -1)
+   		fprintf(stderr, "%s: sthread_create: deposit %s\n", argv[0], strerror(errno));
 
-
-  if (sthread_init() == -1)
-    fprintf(stderr, "%s: sthread_init: %s\n", argv[0], strerror(errno));
-
-  if (sthread_create(&thr2, deposit, (void *)2) == -1)
-    fprintf(stderr, "%s: sthread_create: deposit %s\n", argv[0], strerror(errno));
-
-  if (sthread_create(&thr1, withdraw, (void *)1) == -1)
-    fprintf(stderr, "%s: sthread_create: withdraw %s\n", argv[0], strerror(errno));
+	if (sthread_create(&thr1, withdraw, (void *)1) == -1)
+    		fprintf(stderr, "%s: sthread_create: withdraw %s\n", argv[0], strerror(errno));
 
 
 
-  printf("down ret:%d\n", sthread_sem_down(&test));
-  printf("down ret:%d\n", sthread_sem_down(&test));
-  printf("down ret:%d\n", sthread_sem_down(&test));
+  	 printf("down ret:%d\n", sthread_sem_down(&test));
+ 	 printf("down ret:%d\n", sthread_sem_down(&test));
+ 	 printf("down ret:%d\n", sthread_sem_down(&test));
+ 	 printf("down ret:%d\n", sthread_sem_down(&test));
+ 	 printf("down ret:%d\n", sthread_sem_down(&test));
 
-  printf("down ret:%d\n", sthread_sem_down(&test));
-  printf("down ret:%d\n", sthread_sem_down(&test));
+
+	 sthread_wake(thr1);
+     //  printf("All done.  Balance = %d\n", balance);
+  	 sthread_wake(thr2);
+     //  printf("All done.  Balance = %d\n", balance);
+	 while (1)
+	 {
+		printf("All done.  Balance = %d\n", balance);
+		sleep(1);
+	 }
+/*
+ if (sthread_create(&thr1, threadmain, (void *)1) == -1)
+    fprintf(stderr, "%s: sthread_create: %s\n", argv[0], strerror(errno));
 
 
-sthread_wake(thr1);
-//printf("All done.  Balance = %d\n", balance);
-  sthread_wake(thr2);
-//printf("All done.  Balance = %d\n", balance);
-while (1)
-{
-printf("All done.  Balance = %d\n", balance);
+  if (sthread_create(&thr2, threadmain, (void *)2) == -1)
+    fprintf(stderr, "%s: sthread_create: %s\n", argv[0], strerror(errno));
+
+
+if (sthread_create(&thr3, threadmain, (void *)3) == -1)
+    fprintf(stderr, "%s: sthread_create: %s\n", argv[0], strerror(errno));
+
+if (sthread_create(&thr4, threadmain, (void *)4) == -1)
+    fprintf(stderr, "%s: sthread_create: %s\n", argv[0], strerror(errno));
+
+if (sthread_create(&thr5, threadmain, (void *)5) == -1)
+    fprintf(stderr, "%s: sthread_create: %s\n", argv[0], strerror(errno));
+
+
+
 sleep(1);
-}
-
-
- // sleep(1);
-
-  sthread_wake(thr1);
- // sleep(1);
-  sthread_wake(thr2);
- // sleep(1);
+sthread_wake(thr1);
+sleep(1);
+sthread_wake(thr2);
+sleep(1);
 printf("%s", "\n\n\n");
 
-  sthread_wake(thr1);
-//sleep(1);
-  sthread_wake(thr2);
-//sleep(1);
+sthread_wake(thr1);
+sleep(1);
+sthread_wake(thr2);
+sleep(1);
 sthread_wake(thr3);
-//sleep(1);
+sleep(1);
 sthread_wake(thr4);
-//sleep(1);
+sleep(1);
 sthread_wake(thr5);
-  sleep(1);
-  return 0;
+sleep(1);
+*/
+
+	return 0;
 }
 
