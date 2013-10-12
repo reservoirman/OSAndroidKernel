9308998f3da8f31c189aba2f2daaddc7ede52c37
prj2113 <prj2113@columbia.edu>
2013-10-12 04:08:15 -0400

    Some folders as well

---
+++ hw3/tester/obj/local/armeabi/objs/hw3/hw3.o.d
@@ -0,0 +1,76 @@
+obj/local/armeabi/objs/hw3/hw3.o: hw3.c \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/utsname.h \
+ inject.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/syscall.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/errno.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ /home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/unistd.h
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/utsname.h:
+
+inject.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/sys/syscall.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/errno.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/home/w4118/android-ndk-r9/platforms/android-3/arch-arm/usr/include/asm/unistd.h:
