cc6feab0c4ee05cd5a87e5ff1758a0169a02e70c
Pooja Jain <prj2113@columbia.edu>
2013-10-16 10:13:52 +0000

    sync.c added comments

--- hw3/sync.c
+++ hw3/sync.c
@@ -18,137 +18,120 @@
 
 unsigned long int flag=0;
 
-void insertNode(struct sthread_sem_struct *sem, sthread_t data) {
-    
-          if (sem->sthread_count == 0) {
-	       sem->head = (struct Node*) malloc((size_t)sizeof(struct Node));
-	       		sem->start = sem->head ;
-               sem->head->data = data;
-               sem->head->next = NULL;
-               sem->sthread_count++;
-                
-           } 
-           else 
-	   {
+//inserts a thread to the waiting queue if semaphore count =0
+void insertNode(struct sthread_sem_struct *sem, sthread_t data) 
+{
+    if (sem->sthread_count == 0)
+	{
+	    sem->head = (struct Node*) malloc((size_t)sizeof(struct Node));
+	   	sem->start = sem->head ;
+        sem->head->data = data;
+       	sem->head->next = NULL;
+        sem->sthread_count++;          
+    } 
+    else 
+	{
 		struct Node *temp = (struct Node *) malloc((size_t)sizeof(struct Node));
-                sem->head->next = temp;
-				temp->data = data;
-                temp->next = NULL;
-                sem->head = temp;
-                sem->sthread_count++;
-                
-           }
+        sem->head->next = temp;
+		temp->data = data;
+        temp->next = NULL;
+        sem->head = temp;
+	    sem->sthread_count++;
+    }
 }
 
+//removes a thread from the waiting queue as soon as semaphore count > 0
 struct Node * removeNode(struct sthread_sem_struct *sem) 
-    {
-    struct Node *temp;
-    temp = sem->start;
+{
+	struct Node *temp;
+   	temp = sem->start;
     sem->start = sem->start->next;
     sem->sthread_count--;
     return temp;
 }
 
-/*void traverse(struct sthread_sem_struct *sem) {
-   	struct Node *current;
-    	current = sem->start;
-	if (current == NULL)
-	{
-		printf("Queue is empty!");
-	}
-   	 while(current != NULL) 
-	{
-        printf("current data: %p current node: %p next node: %p\n", current->data, current, current->next);
-        current = current->next;
-    	}
-	printf("\n");
-}
-*/
 
-void lock()
+void lock()													//Its a spin lock used to lock the semaphore implementation
 {
-	while (test_and_set(&flag))
-		;
+	while (test_and_set(&flag));
 }
 void unlock()
 {
 	flag=0;
 }
 
+
+
+// initializes the sem->count to variable count passed by the calling process
 int sthread_sem_init(sthread_sem_t *sem, int count)
 {
 	lock();
-        sem->count = count;
-        sem->head = NULL;
+    sem->count = count;
+    sem->head = NULL;
 	sem->start = NULL ;
-        sem->sthread_count = 0;
-        int ret;
-        if ((sem->count == count) && (sem->head == NULL))
-	{
-	    
-            ret =  0;
+    sem->sthread_count = 0;
+    int ret;
+    if ((sem->count == count) && (sem->head == NULL))
+	{	    
+    	ret =  0;
 	}        
 	else 
 	{	
 	    ret = -1;
 	}
-        unlock();
-
+    unlock();
 	return ret;
 }
 
+
+//Destroys the semaphore by initializing the count to 0 and head to NULL
 int sthread_sem_destroy(sthread_sem_t *sem)
 {
 
 	lock();
-        sem->count = 0;
-        sem->head = NULL;
+    sem->count = 0;
+    sem->head = NULL;
 	int ret;
 	if ((sem->count == 0) && (sem->head == NULL))
-	{
-	    
-            ret =  0;
+	{   
+		ret =  0;
 	}        
 	else 
 	{	
 	    ret = -1;
 	}
-        unlock();
-        return ret;
+    unlock();
+    return ret;
 }
-
+/*
+down() - It checks if sem-> > 0 , If true then decreases the count and gives the semaphore else adds the thread to the waiting queue and suspends it.
+*/
 int sthread_sem_down(sthread_sem_t *sem)
 {
-	
-	
-	lock();	
-	
+	lock();		
 	int ret;
-	
-        if (sem->count > 0)
-	{
-	 
-            sem->count--;
+    if (sem->count > 0)
+	{  
+		sem->count--;
 	    ret = 0;
 	    unlock();
-
 	}	
 	else
 	{
-	 
-          insertNode(sem, sthread_self());
-	  ret = -1;
-	  unlock();
-	  sthread_suspend();  
-        } 
-	
-	return ret;
-       
+      	insertNode(sem, sthread_self());
+	  	ret = -1;
+	  	unlock();
+	  	sthread_suspend();  
+    } 
+	return ret; 
 }
 
+
+/*
+try_down() - It checks if sem-> > 0 , If true then decreases the count else just returns
+*/
 int sthread_sem_try_down(sthread_sem_t *sem)
 {
-	
 	lock();
 	int ret;
 	if (sem->count > 0)
@@ -161,16 +144,15 @@ int sthread_sem_try_down(sthread_sem_t *sem)
 		ret = -1;
 	}
 	unlock();
-        return ret;
+    return ret;
 }
 
-
+/*
+up()-> It is called by a thread when a semaphore is released. If the waiting queue is empty then just increment sem->count else remove and wake up the first thread on the waiting queue.
+*/
 int sthread_sem_up(sthread_sem_t *sem)
-{
-	
+{	
 	lock();
-	
-	
 	if (sem->sthread_count <= 0)
 	{
 		sem->count++;
@@ -184,13 +166,6 @@ int sthread_sem_up(sthread_sem_t *sem)
 		free(longestWaitingThread);
 		unlock();
 		sthread_wake(temp.data);
-		
-		
-	
 	}
-	
 	return 0;
 }       
-
-
-
