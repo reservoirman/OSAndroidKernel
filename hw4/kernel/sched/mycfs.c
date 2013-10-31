ce6f867d2029df8bafb717135f3e767d8edff508
prj2113 <prj2113@columbia.edu>
2013-10-30 20:34:51 -0400

    - Included the skeleton for mycfs.c 	- added mycfs.o to makefile 	- created struct sched_mycfs_entity 	- added an instance of that to task_struct 	- populated hooks to sched_class mycfs_Sched_class 	- added variables to struct mycfs_rq

---
+++ hw4/kernel/sched/mycfs.c
@@ -0,0 +1,32 @@
+#include <linux/sched.h>
+#include "sched.h"
+
+static void dequeue_task_mycfs(struct rq *rq, struct task_struct *prev,int flags)
+{
+}
+
+static void enqueue_task_mycfs(struct rq *rq, struct task_struct *p, int flags)
+{
+}
+//this will be the big one to implement!!!
+static struct task_struct *pick_next_task_mycfs(struct rq *rq)
+{
+	return 0;
+}
+
+static void put_prev_task_mycfs(struct rq *rq, struct task_struct *prev)
+{
+}
+
+const struct sched_class mycfs_sched_class = {
+	.dequeue_task	= dequeue_task_mycfs,
+	.pick_next_task = pick_next_task_mycfs,
+	.enqueue_task = enqueue_task_mycfs,
+	.put_prev_task	= put_prev_task_mycfs,
+	//the other hooks will need to be implemented too
+		
+};
+
+
+
+
