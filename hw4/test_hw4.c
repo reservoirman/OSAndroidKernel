acea1d3452193edb2565a0c97901ece075e009ce
prj2113 <prj2113@columbia.edu>
2013-10-31 01:29:40 -0400

    cleaned the repository

--- hw4/test_hw4.c
+++ hw4/test_hw4.c
@@ -7,7 +7,14 @@ int main()
 {
 	pid_t pid;
 	int check;
+	struct sched_param param;
+	param.sched_priority = 2 ;
 	pid = getpid();
+
+	if (sched_setscheduler(0, SCHED_FIFO, &param) != 0) 
+	{
+    	perror("sched_setscheduler");
+	}
 	check = sched_setscheduler(pid, SCHED_MYCFS,  );
 	printf("return of sched_setscheduler %d\n", check);
 	return 0;
