d6bb225afa5ef09eaff90e8995a2a4702b41a1a6
Ten-Seng Guh <tg2458@columbia.edu>
2013-11-06 16:08:08 -0800

    added init_mycfs_rq call to call RB_ROOT to create the rb tree for MYCFS's rq

--- hw4/kernel/sched/sched.h
+++ hw4/kernel/sched/sched.h
@@ -1170,6 +1170,9 @@ extern void print_rt_stats(struct seq_file *m, int cpu);
 
 extern void init_cfs_rq(struct cfs_rq *cfs_rq);
 extern void init_rt_rq(struct rt_rq *rt_rq, struct rq *rq);
+// TG
+extern void init_mycfs_rq(struct mycfs_rq *mycfs_rq);
+
 extern void unthrottle_offline_cfs_rqs(struct rq *rq);
 
 extern void account_cfs_bandwidth_used(int enabled, int was_enabled);
